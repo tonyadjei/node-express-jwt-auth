55548e12824e0815c7a588596c082babdef7d797
Anthony Teye-Adjei <tonyteyeadjei@gmail.com>
2021-04-30 16:34:43 +0200

    starter files, created routes for authentication  and views, controllers for authRoutes, installed dependencies

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://tonygrace:Tonyadjei2402@nodejs.4fw0l.mongodb.net/node-auth';
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
+// routes
+
+app.get('/', (req,res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes);
