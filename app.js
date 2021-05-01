064aa4fe56e61c252fa1d0e9a4dadfe250d6c2fd
Anthony Teye-Adjei <tonyteyeadjei@gmail.com>
2021-05-01 15:24:58 +0200

    generated JWT on server and sent it via cookies to the client on the browser, jwt(header, payload and signature), secret string used to generate signature

--- app.js
+++ app.js
@@ -27,18 +27,16 @@ app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
 
 // routes for testing cookies
+// app.get('/set-cookies', (req, res) => {
+//     // res.setHeader('Set-Cookie', 'newUser=true');
+//     res.cookie('newUser', false);
+//     res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true }); //maxAge is the expiring data for the cookie, it should be in milliseconds. the secure property ensures that the cookie is sent only if we have an https connection, httpOnly property means that we can only access the cookie via http requests, if we try to access it on the frontend by saying 'document.cookie', we won't see it there. set these properties in production
+//     res.send('you got the cookies!');
 
-app.get('/set-cookies', (req, res) => {
-    // res.setHeader('Set-Cookie', 'newUser=true');
-    res.cookie('newUser', false);
-    res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true }); //maxAge is the expiring data for the cookie, it should be in milliseconds. the secure property ensures that the cookie is sent only if we have an https connection, httpOnly property means that we can only access the cookie via http requests, if we try to access it on the frontend by saying 'document.cookie', we won't see it there. set these properties in production
-    res.send('you got the cookies!');
+// });
 
-});
-
-app.get('/read-cookies', (req, res) => {
-    const cookies = req.cookies;
-    console.log(cookies);
-    res.json(cookies);
-
-});
+// app.get('/read-cookies', (req, res) => {
+//     const cookies = req.cookies;
+//     console.log(cookies);
+//     res.json(cookies);
+// });
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 // handle errors
 const handleErrors = (err) => {
@@ -14,11 +15,20 @@ const handleErrors = (err) => {
     }
     // validation errors
     if(err.message.includes('user validation failed')){
-        Object.values(err.errors).forEach(({properties}) => {
+        Object.values(err.errors).forEach(({properties}) => { //you have to add an extra parentheses when you are deconstructing an object and using that as your 'i' in your forEach()
             errors[properties.path] = properties.message;
         });
     }
     return errors;
+}
+
+// create jwt token
+
+const maxAge = 3 * 24 * 60 * 60; // for jwt, the expiry date is in seconds, for cookies, it's in milliseconds
+const createToken = (id) => {
+    return jwt.sign({ id }, 'fearless pilgrim', { // we put the payload(which is an object) and then a secret(which we will hash together with the header and the payload to create the signature), and then an options object(which we can use to specify the period for which the jwt will be valid by using the expiresIn property ). The header is applied automatically.
+        expiresIn: maxAge
+    });
 }
 
 module.exports.signup_get = (req, res) => {
@@ -31,9 +41,12 @@ module.exports.login_get = (req, res) => {
 
 module.exports.signup_post = async (req, res) => {
     const { email, password } = req.body;
+    // note: when we use async and await to perform asynchronous codes, we cannot use the 'catch()' method to catch errors, instead, we need to surround our await statements in a try...catch block
     try {
-        const user = await User.create({email, password})
-        res.status(201).json(user);
+        const user = await User.create({email, password}); // we get back the new document that has been stored in the database and we are storing it in the 'user' variable.
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+        res.status(201).json({ user: user._id });
     }
     catch(err) {
         const errors = handleErrors(err);
